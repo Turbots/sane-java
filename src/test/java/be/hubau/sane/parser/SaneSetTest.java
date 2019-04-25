7096f3ec4a0325cb4deea68422bcb10d3ce1daf3
Dieter Hubau <dhubau@gmail.com>
2019-04-25 23:16:01 +0200

    Updated test

--- src/test/java/be/hubau/sane/parser/SaneSetTest.java
+++ src/test/java/be/hubau/sane/parser/SaneSetTest.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SaneSetTest {
 
     @Test
     void createSetAndCheckKeys() throws IOException, ParserException {
         SaneSet saneSet = new SaneSet(SaneSetTest.class.getResourceAsStream("/example.sane"));
 
-        System.out.println(saneSet.getKeys());
+        assertEquals("\"SANE Example\"", saneSet.getKeys().get("title"));
     }
 }
